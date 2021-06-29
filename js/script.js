0e237e51a58bb88eb8ea28d01ad786d9ddc929e8
Иван Лихачев <>
2021-06-29 09:45:07 +0500

    2

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// LazyLoad
+var lazyLoadInstance = new LazyLoad({
+});
+
+$(document).ready(function() {
+  $('.toggle-button').click(function() {
+    $('.toggle-block').fadeToggle('slow');
+  });
+});
+
+
+
+
+
+
